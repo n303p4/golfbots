28a2a69b29d14cf17058ee9dfbb30c7c71a047af
n303p4 <n303p4@users.noreply.github.com>
2017-05-30 03:48:01 -0400

    Added the bot itself.

---
+++ tamamo.py
@@ -0,0 +1,20 @@
+import discord # This bot is released into the public domain, under the terms of the Unlicense. See http://unlicense.org/ for details.
+from discord.ext import commands
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("tam ", "tam"))
+bot.description = "This is Tamamo, a discord.py bot written in no more than 20 lines. Using semicolons is cheating."
+bot.check(lambda ctx: not ctx.author.bot) # Prevent the bot from replying to other bots.
+@bot.event # Two-line error handler that avoids unknown command messages and check failure responses.
+async def on_command_error(ctx, err): (await ctx.send(err)) if not isinstance(err, (commands.CommandNotFound, commands.CheckFailure)) else None
+@bot.command(aliases=["pong", "echo", "say"], help="Pings the bot.")
+async def ping(ctx, *, message:str=None): (await ctx.send("\u200b" + message)) if message else (await ctx.send(":ping_pong:"))
+@bot.command(name="user", aliases=["uinfo", "userinfo"], help="Shows information about a user.")
+async def _user(ctx, *, user:discord.Member): await ctx.send(f"**{user.name}#{user.discriminator}** ({user.id})\nNickname: {user.nick}\nBot? {user.bot}\nStatus: {user.status.name}\nJoined guild: {user.joined_at.ctime()}\nJoined Discord: {user.created_at.ctime()}\n{user.avatar_url_as(format='png', size=128)}")
+@bot.command(aliases=["server", "sinfo", "serverinfo", "ginfo", "guildinfo"], help="Shows information about the current guild.")
+async def guild(ctx): await ctx.send(f"**{ctx.guild.name}** ({ctx.guild.id})\nOwner: {ctx.guild.owner.name}#{ctx.guild.owner.discriminator}\nMembers: {len(ctx.guild.members)}\nText channels: {len(ctx.guild.text_channels)}\nVoice channels: {len(ctx.guild.voice_channels)}\nRoles: {len(ctx.guild.roles)}\nCustom emojis: {len(ctx.guild.emojis)}\nRegion: {ctx.guild.region}\n{ctx.guild.icon_url}")
+@bot.command(aliases=["elist"], help="Lists the guild's custom emojis.")
+async def emojis(ctx): (await ctx.send(f"{''.join(list(str(emoji) for emoji in ctx.guild.emojis))}")) if len(ctx.guild.emojis) else (await ctx.send("This guild has no custom emojis."))
+@bot.command(aliases=["xk"], help="Fetch an xkcd comic by number.")
+async def xkcd(ctx, number:int=None): (await ctx.send(f"https://xkcd.com/{number}")) if number else (await ctx.send("https://xkcd.com/"))
+@bot.command(aliases=["shutdown", "kys"], help="Halts the bot.") # Shutdown command that checks for the bot owner without needing an extra line for the check.
+async def halt(ctx): (await bot.logout()) if (await bot.is_owner(ctx.author)) else None
+bot.run(open("oauth.txt").read().strip("\n")) # Token should be stored in oauth.txt. The bot won't run without it.
